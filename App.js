205439757dee02ecca6d834e0ee22d3fef39005d
ryanslam <ryanshiau1@gmail.com>
2020-06-19 22:02:51 -0700

    First Commit

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+          <Text>This is the homepage</Text>
+          <Image source= 
+          {{uri: "https://reactnative.dev/docs/assets/p_cat2.png"}}
+          style={{width: 200, height:200}}/>  
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
